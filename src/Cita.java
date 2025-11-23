5de32d0682cd6e74ecc2f25ca432cb5df31828c0
Mzero11 <mateo.silva@unl.edu.ec>
2025-11-23 13:36:09 -0500

    Creacion

---
+++ src/Cita.java
@@ -0,0 +1,11 @@
+public class Cita {
+    public String id;
+    public String apellido;
+    public String fechaHora;
+
+    public Cita(String id, String apellido, String fechaHora) {
+        this.id = id;
+        this.apellido = apellido;
+        this.fechaHora = fechaHora;
+    }
+}
